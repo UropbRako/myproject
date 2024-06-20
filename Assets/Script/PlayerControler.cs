cc764dad93335153a68ab401e35fc44e1dbd8e93
UropbRako <igopbrakovsky@gmail.com>
2024-06-20 18:47:20 +0700

    addset

---
+++ Assets/Script/PlayerControler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public int speed = 20;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+    }
+}
